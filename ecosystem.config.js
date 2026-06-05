06db4b1e842d3f4004e1e03108cdb3cbbc2962e5
joefany86-design <joefany86@gmail.com>
2026-06-06 05:39:06 +0700

    chore: add web admin panel to pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,5 +10,15 @@ module.exports = {
       autorestart: true,
       watch: false,
     },
+    {
+      name: 'admin-panel-2026',
+      script: './admin-panel/server.js',
+      env: {
+        NODE_ENV: 'production',
+      },
+      max_memory_restart: '200M',
+      autorestart: true,
+      watch: false,
+    }
   ],
 };
