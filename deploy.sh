06d3ad5ba0d0015b1bb057db174098d2d513dd57
joefany86-design <joefany86@gmail.com>
2026-06-08 22:26:27 +0700

    chore: robustify deploy.sh by changing directory to script location first

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "=== Memulai Deploy bot-discord-2026 ==="
 
+# Pindah ke direktori script
+cd "$(dirname "$0")" || exit 1
+
 # Ambil update terbaru dari Git
 git pull origin main
 
