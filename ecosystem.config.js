dba3696878f3fb1bfc60ff94bf18843c74f603d1
joefany86-design <joefany86@gmail.com>
2026-08-31 23:58:56 +0700

    fix ecosystem config to remove admin-panel

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,16 +9,7 @@ module.exports = {
       max_memory_restart: '750M',
       autorestart: true,
       watch: false,
-    },
-    {
-      name: 'admin-panel-2026',
-      script: './admin-panel/server.js',
-      env: {
-        NODE_ENV: 'production',
-      },
-      max_memory_restart: '200M',
-      autorestart: true,
-      watch: false,
     }
   ],
 };
+
